824e06424983b9ea873eb929be443e7162c89e66
Jan Koudela <jan.koudela@manica.cz>
2020-03-03 11:56:16 +0100

    Module cleanup

--- winserver/main.tf
+++ winserver/main.tf
@@ -1,34 +1,33 @@
 
-
 # Create network interface
-resource "azurerm_network_interface" "dc01nic" {
-  name                      = "dc01nic"
-  location                  = azurerm_resource_group.rg.location
-  resource_group_name       = azurerm_resource_group.rg.name
+resource "azurerm_network_interface" "nic" {
+  name                      = "${var.servername}-nic"
+  location                  = var.location
+  resource_group_name       = var.rgname
 
   ip_configuration {
-    name                          = "dc01NICConfg"
-    subnet_id                     = azurerm_subnet.subnet.id
+    name                          = "${var.servername}-niccofg"
+    subnet_id                     = var.subnetid
     private_ip_address_allocation = "dynamic"
-    public_ip_address_id          = azurerm_public_ip.dc01pubip.id
+    # public_ip_address_id          = azurerm_public_ip.dc01pubip.id
   }
 }
 
-resource "azurerm_subnet_network_security_group_association" "aadcas" {
-  subnet_id                 = azurerm_subnet.subnet.id
-  network_security_group_id = azurerm_network_security_group.aadcnsg.id
+resource "azurerm_subnet_network_security_group_association" "nsg" {
+  subnet_id                 = var.subnetid
+  network_security_group_id = var.nsgid
 }
 
 # Create servers
-resource "azurerm_windows_virtual_machine" "dc01" {
-  name                            = "dc01-vm"
-  resource_group_name             = azurerm_resource_group.rg.name
-  location                        = azurerm_resource_group.rg.location
+resource "azurerm_windows_virtual_machine" "vm" {
+  name                            = "${var.servername}-vm"
+  resource_group_name             = var.rgname
+  location                        = var.location
   size                            = "Standard_DS1_v2"
   admin_username                  = "vmadmin"
   admin_password                  = "Password12345+"
   network_interface_ids = [
-    azurerm_network_interface.dc01nic.id,
+    azurerm_network_interface.nic.id,
   ]
 
   source_image_reference {
--- winserver/variables.tf
+++ winserver/variables.tf
@@ -2,3 +2,23 @@ variable "servername" {
     description = "Name of the Server"
     type = string
 }
+
+variable "rgname" {
+    description = "Resource group name"
+    type = string
+}
+
+variable "location" {
+    description = "Location"
+    type = string
+}
+
+variable "subnetid" {
+    description = "ID of the subnet"
+    type = string
+}
+
+variable "nsgid" {
+    description = "ID of the Network Security Group"
+    type = string
+}
